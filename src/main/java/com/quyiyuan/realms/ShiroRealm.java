866ca19fb522fafe8ae5947f906b835bda9c6c52
范守星 <fanshouxing@quyiyuan.com>
2016-12-05 19:16:31 +0800

    first commit

---
+++ src/main/java/com/quyiyuan/realms/ShiroRealm.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016. 上海趣医网络技术服务有限公司 版权所有
+ * Shanghai QuYi Network Technology Co., Ltd. All Rights Reserved.
+ *
+ * This is NOT a freeware,use is subject to license terms.
+ */
+
+package com.quyiyuan.realms;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by Fsx on 2016/12/5.
+ */
+public class ShiroRealm implements Realm {
+    public String getName() {
+        return null;
+    }
+
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
---
+++ src/main/webapp/list.jsp
@@ -0,0 +1,23 @@
+<%--
+  ~ Copyright (c) 2016. 上海趣医网络技术服务有限公司 版权所有
+  ~ Shanghai QuYi Network Technology Co., Ltd. All Rights Reserved.
+  ~
+  ~ This is NOT a freeware,use is subject to license terms.
+  --%>
+
+<%--
+  Created by IntelliJ IDEA.
+  User: Fsx
+  Date: 2016/12/5
+  Time: 18:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>List</title>
+</head>
+<body>
+    <h2>List Page</h2>
+</body>
+</html>
